ce0a96e787991b6f68b2b1e96f5164263948820e
Alex Mason <Axman6@gmail.com>
2022-12-30 03:12:19 +1100

    Initial test program

---
+++ app/Main.hs
@@ -0,0 +1,36 @@
+{-# LANGUAGE LambdaCase #-}
+module Main where
+
+-- import qualified MyLib (someFunc)
+import Data.Aeson.FlatParse (value)
+import Data.ByteString qualified as BS
+import FlatParse.Basic qualified as F
+import Data.Aeson qualified as A
+import Data.Aeson.Types (Value)
+
+import System.Environment (getArgs)
+
+main :: IO ()
+main = do
+  putStrLn "Hello, Haskell!"
+  getArgs >>= \case
+    ["flatparse",fname] -> do
+      putStrLn "Parsing with flatparse"
+      f <- BS.readFile fname
+      case F.runParser value f of
+        F.OK !res other -> putStrLn $ take 100 $ show (res :: Value)
+        e -> print e
+    ["aeson",fname] -> do
+      putStrLn "Parsing with aeson"
+      f <- BS.readFile fname
+      case A.eitherDecodeStrict f of
+        Right !res -> putStrLn $ take 100 $ show (res :: Value)
+        Left err -> print err
+    ["compare",fname] -> do
+      putStrLn "Parsing with aeson"
+      f <- BS.readFile fname
+      case A.eitherDecodeStrict f of
+        Right !ares -> case F.runParser value f of
+          F.OK !fres other -> print (ares == fres)
+          e -> print e
+        Left err -> print err
